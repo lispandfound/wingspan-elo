470d80674c62ee627f6b135dd6c536802df1f663
Jake Faulkner <jakefaulkn@gmail.com>
2024-11-22 20:45:01 +1300

    remove random dist files

--- dist-newstyle/tmp/src-689374/semialign-1.3.1/src/Data/Semialign/Indexed.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE Safe #-}
--- | Zipping and aligning of indexed functors.
-module Data.Semialign.Indexed (
-    SemialignWithIndex (..),
-    ZipWithIndex (..),
-    RepeatWithIndex (..),
-) where
-
-import Data.Semialign.Internal
