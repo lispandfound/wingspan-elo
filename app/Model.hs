d7e6481d432a7eb12da05f371e27849e025b211e
Jake Faulkner <jakefaulkn@gmail.com>
2024-11-23 19:50:25 +1300

    reformat Model.hs

--- app/Model.hs
+++ app/Model.hs
@@ -1,18 +1,18 @@
 module Model where
 
-
-import Data.Text (Text)
 import Control.Monad (void)
-import           Control.Monad.IO.Class  (liftIO, MonadIO)
-import           Control.Monad.Trans.Reader (ReaderT)
-import GHC.Int (Int64)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Trans.Reader (ReaderT)
+import Data.List (find, groupBy, maximumBy, sortOn)
+import Data.Maybe (listToMaybe, mapMaybe)
 import Data.Ord (comparing)
-import Data.List (sortOn, groupBy, maximumBy, find)
-import Data.Maybe (mapMaybe, listToMaybe)
-import           Database.Persist.Sqlite
-import           Database.Persist.TH
+import Data.Text (Text)
+import Database.Persist.Sqlite
+import Database.Persist.TH
 
-share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
 Player
     name Text
     country Text
@@ -41,35 +41,32 @@ Avatar
     name Text
 |]
 
+initializeDB :: (MonadIO m) => ReaderT SqlBackend m ()
+initializeDB = runMigration migrateAll
 
-
-initializeDB :: MonadIO m =>  ReaderT SqlBackend m ()
-initializeDB  = runMigration migrateAll
-  
-getRankings :: MonadIO m =>  ReaderT SqlBackend m [(Key Player, Player, Ranking)]
-getRankings  = do
+getRankings :: (MonadIO m) => ReaderT SqlBackend m [(Key Player, Player, Ranking)]
+getRankings = do
   rankingRecords <- selectList [] []
   players <- selectList [] []
-  let rankings =  sortOn (negate . rankingElo) . map (maximumBy (comparing rankingTimestamp)) . groupBy player . map entityVal $ rankingRecords
-  return $ mapMaybe (\ranking -> fmap (\player -> (( entityKey) player, entityVal player, ranking)) $ find (\player -> entityKey player == rankingPlayerId ranking) players) rankings
-  
+  let rankings = sortOn (negate . rankingElo) . map (maximumBy (comparing rankingTimestamp)) . groupBy player . map entityVal $ rankingRecords
+  return $ mapMaybe (\ranking -> fmap (\player -> ((entityKey) player, entityVal player, ranking)) $ find (\player -> entityKey player == rankingPlayerId ranking) players) rankings
   where
     player (Ranking {rankingPlayerId = id1}) (Ranking {rankingPlayerId = id2}) = id1 == id2
 
-getPlayerRankings :: MonadIO m => Key Player -> ReaderT SqlBackend m [Ranking]
+getPlayerRankings :: (MonadIO m) => Key Player -> ReaderT SqlBackend m [Ranking]
 getPlayerRankings key = map entityVal <$> selectList [RankingPlayerId ==. key] [Desc RankingTimestamp]
 
-playerAvatar :: MonadIO m => Player -> ReaderT SqlBackend m (Maybe Avatar)
+playerAvatar :: (MonadIO m) => Player -> ReaderT SqlBackend m (Maybe Avatar)
 playerAvatar player = get (playerAvatarId player)
 
-dummyRankings :: MonadIO m =>  ReaderT SqlBackend m ()
+dummyRankings :: (MonadIO m) => ReaderT SqlBackend m ()
 dummyRankings = do
   kiwi <- insert $ Avatar "kiwi" "Kiwi"
   kea <- insert $ Avatar "kea" "Kea"
   piwakawaka <- insert $ Avatar "piwakawaka" "Pīwakawaka"
   tui <- insert $ Avatar "tui" "Tūī"
   magnus <- insert $ Player "Magnus Birdman" "NOR" kiwi
-  sofia <- insert $ Player "Sofia Wings" "UK" tui 
+  sofia <- insert $ Player "Sofia Wings" "UK" tui
   ada <- insert $ Player "Ada Aviary" "IND" piwakawaka
   john <- insert $ Player "John Feathers" "CAN" kea
   void . insert $ Ranking 2900.0 0.0 "2024-11-22T00:00:00" magnus
@@ -78,15 +75,14 @@ dummyRankings = do
   void . insert $ Ranking 2730.0 0.0 "2024-11-22T00:00:00" ada
   void . insert $ Ranking 2720.0 0.0 "2024-11-22T00:00:00" john
 
-avatarForKey :: MonadIO m => Text -> ReaderT SqlBackend m (Maybe Avatar)
+avatarForKey :: (MonadIO m) => Text -> ReaderT SqlBackend m (Maybe Avatar)
 avatarForKey key = listToMaybe . map entityVal <$> selectList [AvatarKey ==. key] []
 
-avatarKeyToId :: MonadIO m => Text -> ReaderT SqlBackend m (Maybe (Key Avatar))
+avatarKeyToId :: (MonadIO m) => Text -> ReaderT SqlBackend m (Maybe (Key Avatar))
 avatarKeyToId key = listToMaybe . map entityKey <$> selectList [AvatarKey ==. key] []
 
 defaultRanking :: Key Player -> Ranking
 defaultRanking player = Ranking 25.0 (25.0 / 3) "NOW" player
 
-
-birds :: MonadIO m => ReaderT SqlBackend m [Entity Avatar]
+birds :: (MonadIO m) => ReaderT SqlBackend m [Entity Avatar]
 birds = selectList [] []
